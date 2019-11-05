704cecb308eae13bd9550b926bb079b8e17fb295
ren-hoek <ren-hoek@users.noreply.github.com>
2019-11-05 22:12:45 +0000

    updated create_daex to remove Jenkins built services

--- create_daex.sh
+++ create_daex.sh
@@ -39,27 +39,3 @@ add_folder nginx_nginx /etc/nginx nginx/nginx/ localhost
 add_folder nginx_nginx /usr/share/nginx/html nginx/html/ localhost
 redeploy_stack nginx nginx/docker-compose.yml localhost
 
-# Deploy services
-
-# Create and deploy RQ
-pull_image redis:latest localhost
-tag_image redis:latest docker.service:5000/redis:latest localhost
-build_image docker.service:5000/daex-meta/rqdash redis/ localhost
-deploy_stack redis redis/docker-compose.yml localhost
-
-# Create and deploy MongoDB
-pull_image mongo:latest localhost
-tag_image mongo:latest docker.service:5000/mongo:latest localhost
-pull_image mongo-express:latest localhost
-tag_image mongo-express:latest docker.service:5000/mongo-express:latest localhost
-deploy_stack mongo mongo/docker-compose.yml localhost
-
-# Create and deploy Daex
-build_image docker.service:5000/daex-meta/daexbase daex/daexbase/ localhost
-build_image docker.service:5000/daex-meta/rbase daex/rbase/ localhost
-build_image docker.service:5000/daex-meta/rstudio daex/rstudio/ localhost
-build_image docker.service:5000/daex-meta/shiny daex/shiny/ localhost
-build_image docker.service:5000/daex-meta/jupyterhub daex/jupyterhub/ localhost
-deploy_stack daex daex/docker-compose.yml localhost
-add_daex_user gavin localhost 
-
